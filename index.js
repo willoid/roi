a9c196ad516c16273fe1598520539f60e96f9dd2
WILLOID <socialguille@gmail.com>
2021-03-04 09:43:52 +0100

    added function

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const investment = document.getElementById("investment");
 const income = document.getElementById("income");
 
-function calculateRoi() {
-    let investmentCost = investment.value;
-    let incomeGenerated = income.value;
+document.getElementById("form").addEventListener("submit", calculateRoi);
 
+let investmentCost;
+let incomeGenerated;
+
+function getValue() {
+    investmentCost = investment.value;
+    incomeGenerated = income.value;
+    console.log(investmentCost, incomeGenerated)
+}
+function calculateRoi(e) {
+    e.preventDefault();
     var firstParam = incomeGenerated - investmentCost;
     var result = firstParam / investmentCost;
-    
+    var parsedResult = parseFloat(result).toFixed(2)
+
+    document.getElementById("result").value = `${parsedResult} €`
+
     console.log(result);
     console.log(investmentCost);
     console.log(incomeGenerated);
